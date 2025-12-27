463a316f3efb1e903194c42d7495075b96ae9f83
renteasy905-web <renteasy905@gmail.com>
2025-12-27 20:15:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const shops = [
     items: ["Margherita", "Pepperoni", "Veggie Supreme", "BBQ Chicken"]
   },
   {
-    name: "Fly Pizza",
+    name: "connect with Pizza",
     img: "https://plus.unsplash.com/premium_photo-1679924471091-f7cd7ad90ddf?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     rating: 4.6,
     desc: "100% veg pizzas, light & tasty.",
